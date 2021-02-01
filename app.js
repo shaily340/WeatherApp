db1ce9a2a5d8aaaf53d18d6d94897cae9ebe4e5a
Shaily Agarwal <shailyagarwal340@gmail.com>
2021-02-02 01:20:37 +0530

    Setup app for heroku

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const geocode=require('./utils/geocode');
 app.set("view engine","ejs");
 app.use(express.static('public'));
 
+const port=process.env.PORT|| 3000
+
+
 app.get("",(req,res)=>{
     res.render("home");
 })
@@ -25,6 +28,6 @@ app.get("/weather",(req,res)=>{
 })
 
 
-app.listen(3000,()=>{
+app.listen(port,()=>{
     console.log('Listening!!');
 })
--- public/help.js
+++ public/help.js
@@ -4,7 +4,7 @@ const search=document.querySelector('input');
 weatherForm.addEventListener('submit',(e)=>{
     e.preventDefault();
     const location=search.value;
-    fetch('http://localhost:3000/weather?address='+location).then((response)=>{
+    fetch('/weather?address='+location).then((response)=>{
         response.json().then((data)=>{
             if(data.error){
                document.querySelector('#forecast').innerHTML=data.error;
